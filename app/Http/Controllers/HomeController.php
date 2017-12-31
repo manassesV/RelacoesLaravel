5a3e42c3802acab64732346f12397b6f35926cf4
Manassés <manavitorino@gmail.com>
2017-12-31 17:23:20 -0200

    Has Many and BelongsTo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index(\App\Turma $turma)
     {
-        dd($turma->user()->get()->toArray());
+        //dd($turma->user()->get()->toArray());
     }
 }
